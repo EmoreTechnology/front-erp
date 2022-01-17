64848c5be473efffb75a9ee991151afd21563aec
emore23 <iurymarmore@gmail.com>
2022-01-17 19:38:24 -0300

    create new repository front-erp

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppStyled } from './app.styles.ts';
+
+function App() {
+	return (
+		<AppStyled>
+			<div className="App">
+				<p>hello world</p>
+			</div>
+		</AppStyled>
+	);
+}
+
+export default App;
---
+++ src/pages/app.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const AppStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+	padding: 80px;
+	width: 100%;
+	height: 100%;
+	background-color: black;
+	color: white;
+`;
