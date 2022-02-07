e7c8f251d2e0add1381c685e2e17abe6ce2b836f
EdilsonMello <edilson_mello.oficial@outlook.com>
2022-02-07 15:36:26 -0300

    create register-products route

---
+++ src/pages/cadastro-produtos/register-products.styles.ts
@@ -0,0 +1,14 @@
+// Styles
+import styled from 'styled-components';
+
+export const RegisterProdutsStyled = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+	width:100%;
+
+  p {
+    color: #fff !important;
+    font-size: 56px;
+  }
+`;
---
+++ src/pages/cadastro-produtos/register-products.tsx
@@ -0,0 +1,12 @@
+// Styles
+import { RegisterProdutsStyled } from './register-products.styles';
+
+function RegisterProducts() {
+
+  return (
+    <RegisterProdutsStyled>
+      <p>Register Products</p>
+    </RegisterProdutsStyled>
+  );
+}
+export default RegisterProducts;
