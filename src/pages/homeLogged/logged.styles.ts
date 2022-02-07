685ce91a8fed99d88449f5b269a552ec7c866ef3
EdilsonMello <edilson_mello.oficial@outlook.com>
2022-02-07 17:38:58 -0300

    create navbar component

--- src/pages/homeLogged/logged.styles.ts
+++ src/pages/homeLogged/logged.styles.ts
@@ -19,50 +19,6 @@ export const LoggedStyled = styled.div`
     padding:10px;
   }
   
-  .navbar {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-direction: row;
-    width: 100%;
-    height: 40px;
-    padding: 10px;
-    
-    @media (min-width: 768px) {
-      padding: 15px;
-    }
-    @media (min-width: 1440px) {
-      padding: 25px;
-    }
-    
-    button {
-      background-color: black;
-      border: none;
-    }
-
-    .menu-open,
-    .icon-off {
-      margin: 20px;
-      height: 40px !important;
-      width: 45px !important;
-    }
-
-    .home {
-      margin-left: 100px;
-
-      @media (min-width:768px) {
-        margin-left: 75px;
-      }
-    }
-    
-    svg {
-      color: #fff;
-      width: 47px;
-      height: 42px;
-      margin-right: 15px;      
-    }
-  }
-  
   span {
     display: flex;
     align-items: center;
@@ -88,14 +44,14 @@ export const LoggedStyled = styled.div`
 
   .to-right {
     display: flex;
-    margin: 20px 0 20px 40px;  
+    margin: 25px 0 20px 40px;  
 
     @media (min-width:768px) {    
     margin: 25px 0 20px 210px;
     }
 
     @media (min-width:1440px) {    
-    margin: 30px 0 20px 210px;
+    margin: 25px 0 20px 210px;
     }
   }
 `;
