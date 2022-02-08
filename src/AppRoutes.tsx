58d0a51ed8007c61ce564663c7649f2cc2001401
emore23 <iurymarmore@gmail.com>
2022-02-07 22:49:23 -0300

    feat(auth-user): ajuste na rota de home

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -23,6 +23,8 @@ const AppRoutes = () => {
 	return (
 		<Router>
 			<Routes>
+				<Route path="/" element={<Navigate replace to="/home" />} />
+
 				{currentUser ? (
 					<Route path="/" element={<Navigate replace to="/home" />} />
 				) : (
